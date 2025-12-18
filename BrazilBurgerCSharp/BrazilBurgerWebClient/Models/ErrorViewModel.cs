ced58a796e986a063ec4ba2453d868e724f831e6
Hadja24 <hadja-oumou.bah@ism.edu.sn>
2025-12-18 19:20:07 +0000

    Nouveau projet CSharp

---
+++ BrazilBurgerCSharp/BrazilBurgerWebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BrazilBurgerWebClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
